b970890ac0e07607affcdae3708687ac809f59b4
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-21 00:12:25 +0530

    test: square root function ref: #3

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,9 +6,30 @@ public class CalculatorTest{
     Calculator c = new Calculator();
 
     @Test
-    public void sampleTest(){
+    public void testAdd(){
         double observed = c.add(3.0, 2.0);
         double expected =  5.0;
+        assertEquals(observed, expected, 1e-10);
+    }
+
+    @Test
+    public void testSquareRootPositive(){
+        double observed = c.squareRoot(49.0);
+        double expected = 7.0;
+        assertEquals(observed, expected, 1e-10);
+    }
+
+    @Test
+    public void testSquareRootEdge(){
+        double observed = c.squareRoot(-1.0);
+        double expected = Double.NaN;
+        assertEquals(observed, expected, 1e-10);
+    }
+
+    @Test
+    public void testSquareRootPrecision(){
+        double observed = c.squareRoot(401.0);
+        double expected = 20.024984394500787;
         assertEquals(observed, expected, 1e-10);
     }
 }
