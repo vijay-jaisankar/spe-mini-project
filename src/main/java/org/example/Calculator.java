a62bdbb43e8166c2a1fd3c108868fad8f9cf891c
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-20 19:49:49 +0530

    feat: add function and simple test

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Calculator {
+    // Add function
+    public static double add(double x, double y){
+        double res = x + y;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Java!");
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.example.Calculator;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest{
+    Calculator c = new Calculator();
+
+    @Test
+    public void sampleTest(){
+        double observed = c.add(3.0, 2.0);
+        double expected =  5.0
+        assertEquals(observed, expected, 1e-10);
+    }
+}
