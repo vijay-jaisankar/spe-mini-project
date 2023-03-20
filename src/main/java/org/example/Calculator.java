e331bcf9804570aa55c633d3aaa94d0600e051de
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-03-21 00:03:27 +0530

    feat: square root ref: #3

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,9 +1,32 @@
 package org.example;
 
+import java.lang.Math;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
+
+    // Logger
+    private static Logger logger = LogManager.getLogger(Calculator.class);
+
     // Add function
-    public static double add(double x, double y){
+    public double add(double x, double y){
         double res = x + y;
+        return res;
+    }
+
+    // Square root function
+    public double squareRoot(double x){
+        logger.info("[INPUT SQROOT] - " + x);
+
+        if(x < 0)  {
+            logger.info("[OUTPUT SQROOT] ERROR");
+            return Double.NaN;
+        }
+            
+
+        double res = Math.sqrt(x);
+        logger.info("[OUTPUT SQROOT] " + res);
         return res;
     }
 
